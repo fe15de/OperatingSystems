1d1384d1fb549a4790f82a9c00acc45884f315d4
Federico Perez <federicoperezramirez@gmail.com>
2024-03-05 10:00:26 -0500

    v1.7
    added priority algorithm

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def fifo():
                     else:
                         CPU += 1  # If no process is eligible to run, just increment CPU time
 
-                return render_template("program.html", n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,                AVGTaT = sum(turnaround_time) / n, table = True, name = 'FIFO')
+                return render_template("program.html", n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'FIFO')
                                                 
     return render_template("program.html", n = n, table = False, name = 'FIFO') 
 
@@ -93,12 +93,63 @@ def sjf():
                         processed[min_index] = True
                         NoP -= 1
 
-                return render_template("program.html", n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,                AVGTaT = sum(turnaround_time) / n, table = True, name = 'SFJ')
-    return render_template("program.html", n = n, table = False,name = 'SFJ') 
+                return render_template("program.html", n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'SJF')
+    return render_template("program.html", n = n, table = False,name = 'SJF') 
 
 @app.route("/priority",methods = ['POST', 'GET'])
 def priority():
-    return render_template("priority.html") 
+    if request.method == 'GET':
+        n = 0
+    if request.method == 'POST':
+        n = int(request.form['n'])
+        if n != 0:
+            data = request.form
+            last_data = list(data.items())[-1]
+            if last_data[0] == 'start':
+                arrival_time = [] * n
+                burst_time = [] * n
+                priority = [] * n
+                CPU = 0
+                for i in range(n):
+                    arrival_time.append(int(request.form.get('at' + str(i))))
+                    burst_time.append(int(request.form.get('bt' + str(i))))
+                    priority.append(int(request.form.get('priority' + str(i))))
+                ATt = [0] * n
+                NoP = n  # number of Processes
+                PPt = [0] * n
+                waiting_time = [0] * n
+                turnaround_time = [0] * n
+
+                for i in range(n):
+                    PPt[i] = priority[i]
+                    ATt[i] = arrival_time[i]
+
+                LAT = max(arrival_time)
+                for i in range(n):
+                    if arrival_time[i] > LAT:
+                        LAT = arrival_time[i]
+
+                print("\nProcess execution sequence:")
+                while NoP > 0 and CPU <= 1000:
+                    min_priority = float('inf')  # Initialize with positive infinity
+                    min_priority_index = -1
+                    for i in range(n):
+                        if ATt[i] <= CPU and PPt[i] != (LAT + 10) and PPt[i] < min_priority:
+                            min_priority = PPt[i]
+                            min_priority_index = i
+                            
+                    if min_priority_index == -1:
+                        CPU += 1
+                    else:
+                        print("P{} is running at CPU time {}".format(min_priority_index + 1, CPU))
+                        waiting_time[min_priority_index] = CPU - arrival_time[min_priority_index]
+                        CPU += burst_time[min_priority_index]
+                        turnaround_time[min_priority_index] = CPU - arrival_time[min_priority_index]
+                        ATt[min_priority_index] = LAT + 10
+                        PPt[min_priority_index] = LAT + 10
+                        NoP -= 1
+                return render_template("program.html", prio = priority ,n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'PRIORITY')
+    return render_template("program.html", n = n, table = False,name = 'PRIORITY') 
 
 
 @app.route("/roundRobin",methods = ['POST', 'GET'])
