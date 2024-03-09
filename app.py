7b08e16a331796c150b1342beaafd20110a04db8
Federico Perez <federicoperezramirez@gmail.com>
2024-03-09 00:07:29 -0500

    v2.0
    added graphs for each algorithm , change menu css

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask , render_template, request
+from flask import Flask, render_template, request
+
+from graph import graph , graphRound
 
 app = Flask(__name__)
 
-@app.route("/" ,methods = ['POST', 'GET'])
-def home():
-    return render_template("home.html") 
 
-@app.route("/fifo",methods = ['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    return render_template("home.html")
+
+@app.route("/fifo", methods=['POST', 'GET'])
 def fifo():
     if request.method == 'GET':
         n = 0
@@ -16,8 +19,8 @@ def fifo():
             data = request.form
             last_data = list(data.items())[-1]
             if last_data[0] == 'start':
-                arrival_time = [] * n
-                burst_time = [] * n
+                arrival_time = []
+                burst_time = []
                 CPU = 0
                 for i in range(n):
                     arrival_time.append(int(request.form.get('at' + str(i))))
@@ -26,30 +29,36 @@ def fifo():
                 NoP = n  # number of Processes
                 waiting_time = [0] * n
                 turnaround_time = [0] * n
+                start_time = [0] * n
+                end_time = [0] * n
 
                 for i in range(n):
                     ATt[i] = arrival_time[i]
 
-
-                while NoP > 0 and CPU <= 1000:
+                while NoP > 0 :
                     for i in range(n):
                         if ATt[i] <= CPU:
-                            
                             waiting_time[i] = CPU - arrival_time[i]
+                            start_time[i] = CPU 
                             CPU += burst_time[i]
+                            end_time[i] = CPU
                             turnaround_time[i] = CPU - arrival_time[i]
-                            ATt[i] = float('inf')  # Set arrival time to infinity so that this process won't be selected again
+                            ATt[i] = float('inf')
                             NoP -= 1
-                            
                             break
                     else:
-                        CPU += 1  # If no process is eligible to run, just increment CPU time
+                        CPU += 1
+                
+                graph_base64 = graph('FIFO GRAPH',start_time,end_time,n)
+                    
+                return render_template("program.html", n=n, bt=burst_time, at=arrival_time, wt=waiting_time,
+                                       tat=turnaround_time, AvgWT=round((sum(waiting_time) / n), 2),
+                                       AVGTaT=round((sum(turnaround_time) / n), 2), table=True, name='FIFO',
+                                       graph=graph_base64)
 
-                return render_template("program.html", n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'FIFO')
-                                                
-    return render_template("program.html", n = n, table = False, name = 'FIFO') 
+    return render_template("program.html", n=n, table=False, name='FIFO')
 
-@app.route("/sjf",methods = ['POST', 'GET'])
+@app.route("/sjf", methods=['POST', 'GET'])
 def sjf():
     if request.method == 'GET':
         n = 0
@@ -59,12 +68,14 @@ def sjf():
             data = request.form
             last_data = list(data.items())[-1]
             if last_data[0] == 'start':
-                arrival_time = [] * n
-                burst_time = [] * n
-                CPU = 0
+                arrival_time = []
+                burst_time = []
                 for i in range(n):
                     arrival_time.append(int(request.form.get('at' + str(i))))
                     burst_time.append(int(request.form.get('bt' + str(i))))
+                CPU = 0
+                start_time = [0] * n
+                end_time = [0] * n
                 ATt = [0] * n
                 NoP = n  # number of Processes
                 waiting_time = [0] * n
@@ -74,8 +85,7 @@ def sjf():
                 for i in range(n):
                     ATt[i] = arrival_time[i]
 
-                print("\nProcess execution sequence:")
-                while NoP > 0 and CPU <= 1000:
+                while NoP > 0:
                     min_burst = float('inf')
                     min_index = -1
                     for i in range(n):
@@ -86,17 +96,24 @@ def sjf():
                     if min_index == -1:
                         CPU += 1
                     else:
-                        print("At CPU time {}: Process P{} is running".format(CPU, min_index + 1))
                         waiting_time[min_index] = CPU - arrival_time[min_index]
+                        start_time[min_index] = CPU
                         CPU += burst_time[min_index]
+                        end_time[min_index] = CPU
                         turnaround_time[min_index] = CPU - arrival_time[min_index]
                         processed[min_index] = True
                         NoP -= 1
 
-                return render_template("program.html", n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'SJF')
-    return render_template("program.html", n = n, table = False,name = 'SJF') 
+                graph_base64 = graph('SJF GRAPH',start_time,end_time,n)
 
-@app.route("/priority",methods = ['POST', 'GET'])
+                return render_template("program.html", n=n, bt=burst_time, at=arrival_time, wt=waiting_time,
+                                       tat=turnaround_time, AvgWT=round((sum(waiting_time) / n), 2),
+                                       AVGTaT=round((sum(turnaround_time) / n), 2),
+                                       table=True, name='SJF', graph=graph_base64)
+
+    return render_template("program.html", n=n, table=False, name='SJF')
+
+@app.route("/priority", methods=['POST', 'GET'])
 def priority():
     if request.method == 'GET':
         n = 0
@@ -106,9 +123,9 @@ def priority():
             data = request.form
             last_data = list(data.items())[-1]
             if last_data[0] == 'start':
-                arrival_time = [] * n
-                burst_time = [] * n
-                priority = [] * n
+                arrival_time = []
+                burst_time = []
+                priority = []
                 CPU = 0
                 for i in range(n):
                     arrival_time.append(int(request.form.get('at' + str(i))))
@@ -119,6 +136,8 @@ def priority():
                 PPt = [0] * n
                 waiting_time = [0] * n
                 turnaround_time = [0] * n
+                start_time = [0] * n
+                end_time = [0] * n
 
                 for i in range(n):
                     PPt[i] = priority[i]
@@ -129,44 +148,53 @@ def priority():
                     if arrival_time[i] > LAT:
                         LAT = arrival_time[i]
 
-                print("\nProcess execution sequence:")
                 while NoP > 0 and CPU <= 1000:
                     min_priority = float('inf')  # Initialize with positive infinity
                     min_priority_index = -1
+                    min_arrival_time = float('inf')
                     for i in range(n):
                         if ATt[i] <= CPU and PPt[i] != (LAT + 10) and PPt[i] < min_priority:
                             min_priority = PPt[i]
                             min_priority_index = i
+                            min_arrival_time = arrival_time[i]
+                        elif ATt[i] <= CPU and PPt[i] != (LAT + 10) and PPt[i] == min_priority and arrival_time[i] < min_arrival_time:
+                            min_priority_index = i
+                            min_arrival_time = arrival_time[i]
                             
                     if min_priority_index == -1:
                         CPU += 1
                     else:
-                        print("P{} is running at CPU time {}".format(min_priority_index + 1, CPU))
+                        start_time[min_priority_index] = CPU
                         waiting_time[min_priority_index] = CPU - arrival_time[min_priority_index]
                         CPU += burst_time[min_priority_index]
+                        end_time[min_priority_index] = CPU
                         turnaround_time[min_priority_index] = CPU - arrival_time[min_priority_index]
                         ATt[min_priority_index] = LAT + 10
                         PPt[min_priority_index] = LAT + 10
                         NoP -= 1
-                return render_template("program.html", prio = priority ,n = n,bt = burst_time, at= arrival_time, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'PRIORITY')
-    return render_template("program.html", n = n, table = False,name = 'PRIORITY') 
 
+                graph_base64 = graph('PRIORITY GRAPH',start_time,end_time,n)
 
-@app.route("/roundRobin",methods = ['POST', 'GET'])
+                return render_template("program.html", prio=priority, n=n, bt=burst_time, at=arrival_time, wt=waiting_time,
+                                       tat=turnaround_time, AvgWT=round((sum(waiting_time) / n), 2), AVGTaT=round((sum(turnaround_time) / n), 2),
+                                       table=True, name='PRIORITY', graph=graph_base64)
+
+    return render_template("program.html", n=n, table=False, name='PRIORITY')
+
+@app.route("/roundRobin", methods=['POST', 'GET'])
 def roundRobin():
     if request.method == 'GET':
         n = 0
         time_quantum = 0
     if request.method == 'POST':
-        print(request.form)
         n = int(request.form['n'])
         time_quantum = int(request.form['tq'])
         if n != 0:
             data = request.form
             last_data = list(data.items())[-1]
             if last_data[0] == 'start':
-                arrival_time = [] * n
-                burst_time = [] * n
+                arrival_time = []
+                burst_time = []
                 for i in range(n):
                     arrival_time.append(int(request.form.get('at' + str(i))))
                     burst_time.append(int(request.form.get('bt' + str(i))))
@@ -178,11 +206,12 @@ def roundRobin():
                 nAux = n
                 processed = [False] * n
                 NoP = n
+                execution_sequence = []  # List to store the execution sequence of processes
 
                 while any(remaining_burst_time):
                     
                     if NoP == 0:
-                        processed = [False]* n
+                        processed = [False] * n
                         NoP = nAux
                     min_arrival = float('inf')
                     index = -1
@@ -194,7 +223,6 @@ def roundRobin():
                     if index == -1:
                         CPU += 1
                     else:
-                        print(f"At CPU time {CPU}: Process P{index + 1} is running") 
                         execute_time = min(time_quantum, remaining_burst_time[index])           
                         waiting_time[index] += CPU - arrival_time[index]
                         CPU += execute_time
@@ -206,9 +234,23 @@ def roundRobin():
                         if remaining_burst_time[index] == 0:
                             nAux -= 1 
                         
+                        # Save the process execution in the execution sequence list
+                        if len(execution_sequence) > 0 and execution_sequence[-1][0] == index:
+                            # If the last process in the list is the same as the current one,
+                            # update the finish time of the previous process
+                            execution_sequence[-1] = (index, execution_sequence[-1][1], CPU)
+                        else:
+                            # If it's a new process, add it to the list
+                            execution_sequence.append((index, CPU - execute_time, CPU))
+                
 
-                return render_template("program.html",time_quantum=time_quantum, prio = priority ,n = n,bt = burst_time, at= arrival_time_aux, wt = waiting_time, tat = turnaround_time,AvgWT = sum(waiting_time) /n ,AVGTaT = sum(turnaround_time) / n, table = True, name = 'ROUND ROBIN')
-    return render_template("program.html", time_quantum = time_quantum ,n = n, table = False,name = 'ROUND ROBIN')
+                graph_base64 = graphRound('ROUND ROBIN GRAPH',execution_sequence,n)
+
+                return render_template("program.html", time_quantum=time_quantum, n=n, bt=burst_time, at=arrival_time_aux, wt=waiting_time,
+                                       tat=turnaround_time, AvgWT= round((sum(waiting_time) / n), 2), AVGTaT=round((sum(turnaround_time) / n), 2),
+                                       table=True, name='ROUND ROBIN', graph=graph_base64)
+    return render_template("program.html", time_quantum=time_quantum, n=n, table=False, name='ROUND ROBIN')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
